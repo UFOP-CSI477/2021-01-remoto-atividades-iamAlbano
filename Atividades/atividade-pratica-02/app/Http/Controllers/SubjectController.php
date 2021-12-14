47b569c11f98d31f84fdce9b2238c79d4217a967
Guilherme Albano <GuiAlbano10@gmail.com>
2021-12-14 14:26:13 -0300

    atividade 2 jetstream

---
+++ Atividades/atividade-pratica-02/app/Http/Controllers/SubjectController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subject;
+use App\Http\Requests\StoreSubjectRequest;
+use App\Http\Requests\UpdateSubjectRequest;
+
+class SubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $subjects = Subject::orderBy('name', 'asc')->get();
+        return view('subjects.todos', ['subjects' => $subjects]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+        return view('subjects.novo');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSubjectRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSubjectRequest $request)
+    {
+        
+
+        $subject = new Subject;
+        $subject->name = $request->name;
+        $subject->price = $request->price;
+        $subject->save();
+        return redirect()->route('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subject $subject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subject $subject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSubjectRequest  $request
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSubjectRequest $request, Subject $subject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subject $subject)
+    {
+        //
+    }
+}
