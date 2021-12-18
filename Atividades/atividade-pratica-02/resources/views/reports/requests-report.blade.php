93c6652cdf400143ebfc999a358609c695908c36
Guilherme Albano <GuiAlbano10@gmail.com>
2021-12-18 14:43:42 -0300

    atividade 2 relatórios com valores totais

--- Atividades/atividade-pratica-02/resources/views/reports/requests-report.blade.php
+++ Atividades/atividade-pratica-02/resources/views/reports/requests-report.blade.php
@@ -54,25 +54,56 @@
                     <tr>
                     <th scope="col">Id</th>
                     <th scope="col">Nome</th>
+                    <th class="text-center">Qtde de protocolos</th>
                     <th scope="col">Preço</th>
+                    <th class="text-center">Valor total</th>
                     </tr>
             </thead>
 
+            @php
+            $valorFinal = 0
+            @endphp
+
             <tbody>
                 @foreach($subjects as $subject)
+                @php
+                $quant = 0;
+                $valorTotal = 0;
+                @endphp
                 <tr>
+
+                    @foreach($requests as $request)
+                    
+                        @if($request->subject_id == $subject->id)
+                            @php 
+                                $quant += 1;
+                                $valorTotal += $subject->price;
+                                $valorFinal += $subject->price;
+                            @endphp
+                        @endif
+                    @endforeach
+
                     <th scope="row">{{$subject->id}}</th>
                     <td>{{$subject->name}}</td>
+                    <td class="text-center">{{$quant}}</td>
                     <td>R$ {{$subject->price}}</td>
+                    <td class="text-center">R$ {{$valorTotal}}</td>
                 </tr>
                 @endforeach
             </tbody>
+
+            <tfoot>
+                    <td class="text-end" colspan="5">Valor final: R$ {{$valorFinal}}</td>      
+            </tfoot>
 
             </table>
         </div>
 
     </div>
+
 </div>
+
+
 
  
 @endsection
