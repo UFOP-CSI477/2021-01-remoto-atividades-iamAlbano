737ac2196622142a7033d3768091c3166e5aa753
Guilherme Albano <GuiAlbano10@gmail.com>
2022-01-03 15:47:18 -0300

    Update cadastro-vendas.blade.php

--- Projeto/resources/views/create/cadastro-vendas.blade.php
+++ Projeto/resources/views/create/cadastro-vendas.blade.php
@@ -9,7 +9,7 @@
             <div class="modal-body">
                 <ul class="nav abas-cadastro nav-pills mb-3" id="pills-tab" role="tablist">
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="pills-compra-tab" data-bs-toggle="pill" data-bs-target="#pills-compra" type="button" role="tab" aria-controls="pills-home" aria-selected="true">
+                        <button class="nav-link active" id="pills-venda-tab" data-bs-toggle="pill" data-bs-target="#pills-venda" type="button" role="tab" aria-controls="pills-home" aria-selected="true">
                         <svg class="crud-icon me-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                         </svg>Venda</button>
